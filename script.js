eeb3ee70cf786206b98db237f212711a696deadc
ZephirXÖR <113257724+ZephirXD@users.noreply.github.com>
2023-12-13 21:07:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+    Particles.init({
+      selector: '.background',
+      maxParticles: 70,
+      connectParticles: 'true',
+      speed:0.5,
+      minDistance: 100,
+      sizeVariations: 3,
+      color: '#B6BBC4'
+    });
+  }
